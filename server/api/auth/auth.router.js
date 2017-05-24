f0164bb53b70c389de966cc64409aa358f33e4ce
aperez25 <alice.perez25@gmail.com>
2017-05-24 07:25:09 -0700

    google auth working

--- server/api/auth/auth.router.js
+++ server/api/auth/auth.router.js
@@ -8,27 +8,26 @@ router.get('/me', function (req, res, next){
 // Google authentication and login
 router.get('/google', passport.authenticate('google', { scope: 'email' }));
 
-// ROUTING IS GOING TO GOOGLE AUTH BUT NOT GETTING A CALLBACK
+// ROUTING IS GOING TO GOOGLE AUTH BUT NOT GETTING THROUGH TO CALLBACK
 
 // handle the callback after Google has authenticated the user
 router.get('/google/callback',
-  passport.authenticate('google', {
-    successRedirect: '/', // or wherever
-    failureRedirect: '/' // or wherever
-  })
-);
+  passport.authenticate('google',
+  function(req, res) {
+    // res.redirect('/users/' + req.user.username);
+  }
+));
 
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 passport.use(
   new GoogleStrategy({
     clientID: '150144051248-7uvcv3k0k299vohquilhah21vf791e0j.apps.googleusercontent.com',
     clientSecret: 'u_ReuHgIeknL_78IIgtdH0KM',
-    callbackURL: 'http://localhost:8080/'
+    callbackURL: 'http://localhost:8080/api/auth/google/callback'
   },
   // Google will send back the token and profile
   function (token, refreshToken, profile, done) {
-    console.log('---', 'in verification callback', profile, '---');
-done();
+
     // the callback will pass back user profile information and each service (Facebook, Twitter, and Google) will pass it back a different way. Passport standardizes the information that comes back in its profile object.
 
   })
